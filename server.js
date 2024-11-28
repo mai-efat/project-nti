cd2526c255e4df22dd26e05a6ebd6060e7b45611
Mai EFAT <132103597+mai-efat@users.noreply.github.com>
2024-11-28 15:43:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mongoose = require("mongoose");
 const port = 3001;
 const routes = require("./routes");
-const mongoUri = "mongodb://root:Password@mongo:27017/todos?authSource=admin";
+const mongoUri = "mongodb://mongodb-service-0.mongodb-service.default.svc.cluster.local:27017;
 
 main().catch((err) => console.log(err));
 
